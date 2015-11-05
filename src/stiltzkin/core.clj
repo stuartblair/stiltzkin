c01a0b0a076d172caaed6c2cc4ee6d3f2a04f64c
Stuart Blair <stuart_blair@gap.com>
2015-11-05 09:42:14 -0800

    Basic functions to control spacing around words and using the joys of juxt to create domains for the permutations.

--- src/stiltzkin/core.clj
+++ src/stiltzkin/core.clj
@@ -4,12 +4,32 @@
             [clojure.string :as str]))
 
 ;; Words may have started with a capital letter or not
-;; Words may have had a space between them or not
+;; Words may have had a leading space or not
+;; Words may have had a trailing space or not 
+;; Passphrase may have had a leading space
 ;; Passphrase may have had a period
 ;; Passphrase may have had a newline at the end or not
 ;; Passphrase may have had a carriage return at the end or not
 ;; Passphrase may have had a space at the end or not
 
+(def with-trailing-space
+  (fn [w]
+    (str w " ")))
+
+(def with-leading-space
+  (fn [w]
+    (str " " w)))
+
+(def as-is
+  (fn [w]
+    w))
+
+(def possibilities
+  (juxt with-leading-space as-is with-trailing-space))
+
+
+
+(possibilities "stuart")
 (str/lower-case "HELLO")
 (str/upper-case "Hello")
 
