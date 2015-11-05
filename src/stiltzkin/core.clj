0d493cc7e7ae03d55ff5ae78576168a57e365be7
Stuart Blair <stuart_blair@gap.com>
2015-11-04 17:41:48 -0800

    Adding import for String functions and starting to sketch out the requirements of my wacky passphrase conundrum.

--- src/stiltzkin/core.clj
+++ src/stiltzkin/core.clj
@@ -1,8 +1,19 @@
 (ns stiltzkin.core
   (:gen-class)
-  (:require [clojure.math.combinatorics :as comb]))
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.string :as str]))
+
+;; Words may have started with a capital letter or not
+;; Words may have had a space between them or not
+;; Passphrase may have had a period
+;; Passphrase may have had a newline at the end or not
+;; Passphrase may have had a carriage return at the end or not
+;; Passphrase may have had a space at the end or not
+
+(str/lower-case "HELLO")
+(str/upper-case "Hello")
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Generates possible combinations for a passphrase given the ordered list of element words"
   [& args]
   (println "Hello, World!"))
