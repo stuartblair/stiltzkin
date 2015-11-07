6352c863f0e10d9d3064cad0c9dd5ad256554a71
Stuart Blair <stuart_blair@gap.com>
2015-11-06 16:38:19 -0800

    Adding nice formatting to the output. Now I just need to add some mutation with respect to whether there's a newline, newline and carriage returns, no newline or CR etc. Dammit, I will catch me that chicken Mr Harry.

--- src/stiltzkin/core.clj
+++ src/stiltzkin/core.clj
@@ -24,26 +24,15 @@
   (fn [w]
     w))
 
-;; Juxt should be fed all the functions that will mutate. However, I reserve
-;; the right to create that list of functions dynamically.
-
-;; Testing to see if that would work.
 (def whitespace-mutators [with-leading-space as-is with-trailing-space])
+
 (def capitalization-mutators [str/lower-case str/upper-case str/capitalize])
-;; What if I use the combinators package to create the caresian product of
-;; the set of functions dealing with spacing and the set of functions that
-;; deal with capitalization?
 
 (def composed
   (fn [args]
     (apply comp args)))
 
 (def mutators (map composed (combo/cartesian-product whitespace-mutators capitalization-mutators)))
-
-;; I'm assuming there's duplication here that will need to be removed from the
-;; functions and so (with-leading-space (str/upper-case w))
-;; will be commutative. Then again, maybe it will try and capitalize the
-;; leading whitespace. That's one to check.
 
 (def word-variations
   (apply juxt mutators))
@@ -55,4 +44,4 @@
 (defn -main
   "Generates possible combinations for a passphrase given the ordered list of element words"
   [& args]
-  (println (map str/join (phrase-variations args))))
+  (doseq [passphrase-attempt (map str/join (phrase-variations args))] (println passphrase-attempt)))
